9e9919657c60c1ec502b1488ea0cc8b912c56a1b
0tak2 <0tak2.code@gmail.com>
2024-12-23 08:46:38 +0900

    Initial Commit

---
+++ kkumku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  kkumku
+//
+//  Created by 임영택 on 12/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ kkumkuTests/kkumkuTests.swift
@@ -0,0 +1,17 @@
+//
+//  kkumkuTests.swift
+//  kkumkuTests
+//
+//  Created by 임영택 on 12/23/24.
+//
+
+import Testing
+@testable import kkumku
+
+struct kkumkuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
