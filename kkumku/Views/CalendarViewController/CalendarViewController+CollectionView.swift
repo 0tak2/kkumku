680f1950817c8d59873f631c3e75779698f55f11
임영택 <0tak2.code@gmail.com>
2025-01-07 18:32:08 +0900

    Feat: 캘린더 보기 (#21)
    * feat: add FSCalendar
    
    * feat: 일자별 꿈 조회
    
    * feat: 선택시 디테일뷰로 이동

---
+++ kkumku/Views/CalendarViewController/CalendarViewController+CollectionView.swift
@@ -0,0 +1,124 @@
+//
+//  CalendarViewController+CollectionView.swift
+//  kkumku
+//
+//  Created by 임영택 on 1/7/25.
+//
+
+import UIKit
+
+extension CalendarViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Section, Item>
+    
+    enum Section: Int {
+        case main
+    }
+    
+    enum Item: Hashable {
+        case dreamCell(Dream)
+        case labelCell(String)
+    }
+    
+    func initCollectionView() {
+        initLayout()
+        registerCells()
+        setDataSource()
+        
+        collectionView.delegate = self
+    }
+    
+    private func initLayout() {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.interGroupSpacing = 12
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        collectionView.collectionViewLayout = layout
+    }
+    
+    private func registerCells() {
+        dreamCellRegistraion = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { cell, indexPath, item in
+            if case let .dreamCell(dream) = item {
+                var content = cell.defaultContentConfiguration()
+                content.text = "\(dream.memo)"
+                content.textProperties.numberOfLines = 3
+                content.secondaryText = "\(dream.dreamClass.descriptionEmoji()) \(dream.startAt.localizedString) ~ \(dream.endAt.localizedStringNoYear)"
+
+                cell.contentConfiguration = content
+                cell.contentView.backgroundColor = .systemGray6
+            }
+        }
+        
+        labelCellRegistraion = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { cell, indexPath, item in
+            if case let .labelCell(labelText) = item {
+                var content = cell.defaultContentConfiguration()
+                content.text = labelText
+                cell.contentConfiguration = content
+            }
+        }
+    }
+    
+    private func setDataSource() {
+        dataSource = DataSource(collectionView: collectionView, cellProvider: { [weak self] collectionView, indexPath, item in
+            guard let dreamCellRegistraion = self?.dreamCellRegistraion,
+                  let labelCellRegistraion = self?.labelCellRegistraion else {
+                return UICollectionViewCell()
+            }
+            
+            if case .dreamCell = item {
+                return collectionView.dequeueConfiguredReusableCell(using: dreamCellRegistraion, for: indexPath, item: item)
+            }
+            
+            if case .labelCell = item {
+                return collectionView.dequeueConfiguredReusableCell(using: labelCellRegistraion, for: indexPath, item: item)
+            }
+            
+            return UICollectionViewCell()
+        })
+    }
+    
+    func applySnapshot(dreams: [Dream]) {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        
+        if !dreams.isEmpty {
+            let items: [Item] = dreams.map { dream in
+                Item.dreamCell(dream)
+            }
+            
+            snapshot.appendItems(items, toSection: .main)
+        } else {
+            snapshot.appendItems([.labelCell("저장된 꿈이 없습니다.")], toSection: .main)
+        }
+        
+        dataSource.apply(snapshot)
+    }
+}
+
+extension CalendarViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if let selectedDate = selectedDate, let currentDreams = dreamsForDay[selectedDate] {
+            return !currentDreams.isEmpty // 레이블이 노출되어 있는 경우 선택 안됨. 꿈인 경우 선택됨
+        }
+        
+        return false
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let selectedDate = selectedDate, let currentDreams = dreamsForDay[selectedDate] else {
+            return
+        }
+        
+        let storyboard = UIStoryboard(name: "DetailDreamView", bundle: nil)
+        guard let detailViewController = storyboard.instantiateViewController(identifier: "DetailDreamViewController")
+                as? DetailDreamViewController else { return }
+        detailViewController.dream = currentDreams[indexPath.item]
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
+}
--- kkumku/Views/CalendarViewController/CalendarViewController.swift
+++ kkumku/Views/CalendarViewController/CalendarViewController.swift
@@ -6,11 +6,160 @@
 //
 
 import UIKit
+import FSCalendar
 
 class CalendarViewController: UIViewController {
-
+    @IBOutlet weak var calendarView: FSCalendar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dataSource: DataSource!
+    var dreamCellRegistraion: UICollectionView.CellRegistration<UICollectionViewListCell, Item>!
+    var labelCellRegistraion: UICollectionView.CellRegistration<UICollectionViewListCell, Item>!
+    
+    let dreamRepository: DreamRepository = DreamRepository.shared
+    var dreamsForDay: [Date: [Dream]] = [:]
+    var selectedDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.title = "꿈 달력"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // MARK: - Calendar
+        calendarView.delegate = self
+        calendarView.dataSource = self
+        
+        // appearance
+        calendarView.backgroundColor = .systemGray6
+        calendarView.appearance.headerTitleColor = .primary
+        calendarView.appearance.headerTitleFont = .systemFont(ofSize: 16, weight: .semibold)
+        calendarView.appearance.weekdayTextColor = .primary
+        calendarView.appearance.weekdayFont = .systemFont(ofSize: 16, weight: .semibold)
+        
+        calendarView.appearance.titleDefaultColor = .white
+        calendarView.appearance.titleSelectionColor = .black
+        calendarView.appearance.selectionColor = .primary
+        
+        calendarView.appearance.subtitleDefaultColor = .lightGray
+        calendarView.appearance.subtitleSelectionColor = .darkGray
+        
+        // locale
+        calendarView.locale = Locale(identifier: "ko_KR")
+        calendarView.appearance.headerDateFormat = "YYYY년 MM월"
+        calendarView.calendarWeekdayView.weekdayLabels[0].text = "일"
+        calendarView.calendarWeekdayView.weekdayLabels[1].text = "월"
+        calendarView.calendarWeekdayView.weekdayLabels[2].text = "화"
+        calendarView.calendarWeekdayView.weekdayLabels[3].text = "수"
+        calendarView.calendarWeekdayView.weekdayLabels[4].text = "목"
+        calendarView.calendarWeekdayView.weekdayLabels[5].text = "금"
+        calendarView.calendarWeekdayView.weekdayLabels[6].text = "토"
+        
+        // init
+        calendarView.select(Date.now)
+        
+        // MARK: - CollectionView
+        initCollectionView()
+        loadDreamsOfCurrentMonth()
+        displayDreams(of: Date.now)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        loadDreamsOfCurrentMonth()
+        if let selectedDate = selectedDate {
+            displayDreams(of: selectedDate)
+        }
+    }
+    
+    private func displayDreams(of date: Date) {
+        if let normalizedDate = truncateHours(of: date) {
+            let dreamsThatDay = dreamsForDay[normalizedDate] ?? []
+            applySnapshot(dreams: dreamsThatDay)
+            selectedDate = normalizedDate
+        } else {
+            applySnapshot(dreams: [])
+        }
+    }
+    
+    private func loadDreamsOfCurrentMonth() {
+        // find dreams in this month
+        let date = calendarView.currentPage
+        Log.debug("will load dreams in \(date)")
+        
+        let components = Calendar.current.dateComponents([.year, .month], from: date)
+        
+        guard let year = components.year, let month = components.month,
+              let firstDay = Date.firstDayOfMonth(year: year, month: month),
+              let lastDay = Date.lastDayOfMonth(year: year, month: month) else {
+            
+            Log.debug("선택된 월의 1일과 말일을 알 수 없습니다")
+            return
+        }
+        
+        let dreams = dreamRepository.findAll(from: firstDay, to: lastDay)
+        
+        // allocate to dreamsForDay
+        self.dreamsForDay = dreams.reduce(into: [:]) { dict, dream in
+            var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: dream.endAt)
+            dateComponents.hour = 0
+            dateComponents.minute = 0
+            dateComponents.second = 0
+            dateComponents.nanosecond = 0
+            
+            if let date = Calendar.current.date(from: dateComponents) {
+                dict[date, default: []].append(dream)
+            }
+        }
+    }
+    
+    private func truncateHours(of date: Date) -> Date? {
+        var dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: date)
+        dateComponents.hour = 0
+        dateComponents.minute = 0
+        dateComponents.second = 0
+        dateComponents.nanosecond = 0
+        
+        return Calendar.current.date(from: dateComponents)
+    }
+}
+
+extension CalendarViewController: FSCalendarDelegate {
+    func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
+        Log.debug("didSelet data \(date)")
+        
+        displayDreams(of: date)
+    }
+    
+    func calendarCurrentPageDidChange(_ calendar: FSCalendar) {
+        loadDreamsOfCurrentMonth()
+        calendar.reloadData()
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) { [weak self] in
+            guard let calendar = self?.calendarView else { return }
+            
+            calendar.select(calendar.currentPage) // 이번 달 1일로 선택
+            self?.selectedDate = calendar.currentPage
+            self?.displayDreams(of: calendar.currentPage)
+        }
+    }
+    
+    func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
+        let shouldSelectDateComponents = Calendar.current.dateComponents([.month], from: date)
+        let monthOfShouldSelect = shouldSelectDateComponents.month
+        
+        let currentDateComponents = Calendar.current.dateComponents([.month], from: calendar.currentPage)
+        let monthOfCurrentPage = currentDateComponents.month
+        
+        return monthOfShouldSelect == monthOfCurrentPage // 이전달, 다음달 일자의 경우 선택되지 않음
+    }
+}
+
+extension CalendarViewController: FSCalendarDataSource {
+    func calendar(_ calendar: FSCalendar, subtitleFor date: Date) -> String? {
+        if let date = truncateHours(of: date), let dreams = dreamsForDay[date], !dreams.isEmpty {
+            return "●"
+        } else {
+            return nil
+        }
     }
 }
