c1d87090a5653138ba600918cd508cd386eb9a81
0tak2 <0tak2.code@gmail.com>
2025-01-16 14:46:19 +0900

    feat: 새 꿈에서 취침 시간에 유저 설정 취침 시간 적용

--- kkumku/Views/Edit/EditViewController.swift
+++ kkumku/Views/Edit/EditViewController.swift
@@ -11,7 +11,29 @@ class EditViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var isInsertingNewDream = true
-    var workingDream: Dream = Dream(startAt: Date.now, endAt: Date.now, memo: "", dreamClass: .auspicious, isLucid: false)
+    var workingDream: Dream = {
+        let settings = UserSettings.shared
+        let calendar = Calendar.current
+        
+        let bedTimeStrFallback = Date.fromHourAndMinute(hour: 22, minute: 00)!.toISOString()
+        let bedTimeRaw = settings.string(.bedTime, or: bedTimeStrFallback)
+        let userBedTime = Date.fromISOString(isoString: bedTimeRaw)!
+        let (hour, minute) = (userBedTime.toHourAndMinute()[0], userBedTime.toHourAndMinute()[1])
+        
+        let bedTimeToShow: Date
+        if let todayBedTime = Date.fromHourAndMinute(hour: hour, minute: minute) {
+            if todayBedTime <= Date.now { // 오늘 일자의 취침 시각이 현재보다 과거라면 바로 적용
+                bedTimeToShow = todayBedTime
+            } else { // 오늘 일자의 취침 시간이 현재보다 미래라면 어제의 취침 시간을 적용
+                let yesterDayBedTime = calendar.date(byAdding: .day, value: -1, to: todayBedTime)
+                bedTimeToShow = yesterDayBedTime ?? Date.now
+            }
+        } else {
+            bedTimeToShow = Date.now // 문제가 있다면 현재로 적용
+        }
+        
+        return Dream(startAt: bedTimeToShow, endAt: Date.now, memo: "", dreamClass: .auspicious, isLucid: false)
+    }()
     
     var isEditStarted: Bool = false {
         didSet {
