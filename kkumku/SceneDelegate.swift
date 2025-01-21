18b1d0feef9cfe19c440935c6dfd66996bd89557
0tak2 <0tak2.code@gmail.com>
2025-01-21 14:09:48 +0900

    feat: SceneDelegate에서 온보딩 여부를 확인해 온보딩 뷰나 Main 스토리보드를 띄우도록 처리

--- kkumku/SceneDelegate.swift
+++ kkumku/SceneDelegate.swift
@@ -13,12 +13,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.windowScene = windowScene
+        window?.backgroundColor = .systemBackground
         self.window?.overrideUserInterfaceStyle = .dark
+        
+        if UserSettings.shared.bool(.onboardingCompleted) {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            window?.rootViewController = storyboard.instantiateInitialViewController()
+        } else {
+            window?.rootViewController = OnboardingViewController()
+        }
+
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
